ee2edf94a2dd4f8313c1bb07f5dcef2f542d3b00
pedroobando <pedroobando@hotmail.com>
2020-10-06 08:17:35 -0400

    types - redux

---
+++ src/redux/types/types.js
@@ -0,0 +1,13 @@
+export const typesAuth = {
+  authStartLogin: '[auth] Start Login',
+  authLogin: '[auth] Login',
+
+  authStartRegister: '[auth] Start Register',
+  authFrogetPassword: '[auth] Renew password',
+  authLogout: '[auth] Logout',
+};
+
+export const typeModal = {
+  uiOpenModal: '[ui] Open Modal',
+  uiCloseModal: '[ui] Close Modal',
+};
