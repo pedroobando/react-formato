d39b309a5ca877299ad1e812018743cd67b1492b
pedroobando <pedroobando@hotmail.com>
2020-10-12 15:26:52 -0400

    salidamodal

--- src/components/salida/SalidaModal.jsx
+++ src/components/salida/SalidaModal.jsx
@@ -16,24 +16,166 @@ const customStyles = {
 Modal.setAppElement('#root');
 
 export const SalidaModal = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(true);
+  const [tabSelect, setTabSelect] = useState(1);
+  // const [modalIsOpen, setModalIsOpen] = useState(true);
 
   const handleModalClose = () => {
-    // console.log(modalIsOpen);
-    setModalIsOpen(!modalIsOpen);
+    // setModalIsOpen(!modalIsOpen);
   };
+
+  const handleTabPeople = (tabs) => {
+    setTabSelect(tabs);
+  };
+
+  const tabMaterial = () => (
+    <section className="border-bottom border-left border-right p-3 mb-5 bg-white rounded-bottom">
+      <div className="form-label-group">
+        <input
+          type="text"
+          id="inputMaterial"
+          className="form-control"
+          placeholder="Material o Equipo"
+          autoComplete="off"
+          required
+        />
+        <label for="inputMaterial">Material o Equipo</label>
+      </div>
+
+      <div className="form-label-group">
+        <input
+          type="text"
+          id="inputMotivol"
+          className="form-control"
+          placeholder="Motivo de Salida"
+          autoComplete="off"
+          required
+        />
+        <label for="inputMotivo">Motivo de Salida</label>
+      </div>
+
+      <div className="form-label-group">
+        <input
+          type="text"
+          id="inputDestino"
+          className="form-control"
+          placeholder="Destino del equipo / material"
+          autoComplete="off"
+          required
+        />
+        <label for="inputDestino">Destino del equipo / material</label>
+      </div>
+
+      <div className="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="checkRetornara" /> Retornara
+        </label>
+      </div>
+    </section>
+  );
+
+  const tabSolicitante = () => (
+    <section className="border-bottom border-left border-right p-3 mb-5 bg-white rounded-bottom">
+      <div className="form-label-group">
+        <input
+          type="text"
+          id="inputPersona"
+          className="form-control"
+          placeholder="CI o RIF - Persona Solicitante"
+          required
+        />
+        <label for="inputPersona">CI o RIF - Persona Solicitante</label>
+      </div>
+      <label for="inputPersonaNombre">Juan Martinez</label>
+
+      <div className="form-label-group">
+        <input
+          type="text"
+          id="inputTransporte"
+          className="form-control"
+          placeholder="Transporte - Placa"
+          required
+        />
+        <label for="inputMotivo">Transporte - Placa</label>
+      </div>
+      <label for="inputPersonaNombre">Datos del Vehiculo</label>
+    </section>
+  );
+
+  const tabAprobadores = () => (
+    <section className="border-bottom border-left border-right p-3 mb-5 bg-white rounded-bottom">
+      <div className="form-label-group">
+        <input
+          type="text"
+          id="inputAprobadorAdm"
+          className="form-control"
+          placeholder="CI. Aprobador Supervisor"
+          required
+        />
+        <label for="inputAprobadorAdm">CI. Aprobador Supervisor</label>
+      </div>
+      <label for="inputPersonaNombre">Juan Martinez</label>
+
+      <div className="form-label-group">
+        <input
+          type="text"
+          id="inputAprobadorSeg"
+          className="form-control"
+          placeholder="CI. Aprobador Proct y Bienes"
+          required
+        />
+        <label for="inputAprobadorSeg">CI. Aprobador Proct y Bienes</label>
+      </div>
+      <label for="inputPersonaNombre">Datos del Vehiculo</label>
+    </section>
+  );
+
   return (
     <div>
       <Modal
-        isOpen={modalIsOpen}
+        isOpen={true}
         onRequestClose={handleModalClose}
         style={customStyles}
         closeTimeoutMS={200}
         className="modal"
         overlayClassName="modal-fondo">
-        <form>
-          <h6>Orden Salida</h6>
-          <hr />
+        <form className="p-2">
+          <ul className="nav nav-tabs">
+            <li onClick={(event) => handleTabPeople(1)} className="nav-item">
+              <button
+                type="button"
+                className={tabSelect === 1 ? 'nav-link active' : 'nav-link '}>
+                Eqp / Material
+              </button>
+            </li>
+            <li onClick={(event) => handleTabPeople(2)} className="nav-item">
+              <button
+                type="button"
+                className={tabSelect === 2 ? 'nav-link active' : 'nav-link'}>
+                Solicitante
+              </button>
+            </li>
+            <li onClick={(event) => handleTabPeople(3)} className="nav-item">
+              <button
+                type="button"
+                className={tabSelect === 3 ? 'nav-link active' : 'nav-link'}>
+                Aprobadores
+              </button>
+            </li>
+          </ul>
+
+          {tabSelect === 1 && tabMaterial()}
+          {tabSelect === 2 && tabSolicitante()}
+          {tabSelect === 3 && tabAprobadores()}
+
+          <div className="d-flex justify-content-around">
+            <button className="btn btn-outline-secondary" type="button">
+              Verificar
+            </button>
+
+            <button className="btn btn-primary" type="submit">
+              Aceptar
+            </button>
+          </div>
         </form>
       </Modal>
     </div>
